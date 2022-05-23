471cb08d97f2d45623f0c29f243f32bf234ebc70
lisamarienagel <linagelma@gmail.com>
2022-05-23 16:06:52 +0200

    pushed funtimes

---
+++ funtimes/lib.py
@@ -0,0 +1,4 @@
+def gossip(name, name2, color, noun, smiley_shortcut):
+    return f"Did you know that {name} cannot stand {name2} because she/he is jealous of their {color} {noun} {smiley_shortcut}."
+
+gossip("Lisa", "Sophie", "purple", "jacket", ":)")
